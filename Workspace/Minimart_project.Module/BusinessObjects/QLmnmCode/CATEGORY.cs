1e0c4f539dca7b4108a0b9505902bacefb919095
Nhat <minhnhatngo700@gmail.com>
2024-10-19 09:03:16 +0700

    new

---
+++ Workspace/Minimart_project.Module/BusinessObjects/QLmnmCode/CATEGORY.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+namespace Minimart_project.Module.BusinessObjects.QLmnm
+{
+
+    public partial class CATEGORY
+    {
+        public CATEGORY(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
---
+++ Workspace/Minimart_project.Module/BusinessObjects/QLmnmCode/ORDER_DETAIL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+namespace Minimart_project.Module.BusinessObjects.QLmnm
+{
+
+    public partial class ORDER_DETAIL
+    {
+        public ORDER_DETAIL(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
